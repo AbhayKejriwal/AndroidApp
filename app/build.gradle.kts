52b1163a2d82dff8417ebbd6e34530881d5504b2
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-06-21 09:33:17 +0530

    Create and launch new activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,12 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+//    implementation(libs.androidx.activity:activity:1.9.0)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//    implementation(libs.com.squareup.retrofit2)
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,30 +6,49 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickHandler", "button clicked")
         //val dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"))
-        val webIntent: Intent = Intent(/* action = */
-            ACTION_VIEW, /* uri = */
-            Uri.parse("https://www.yahoo.com")
-        ).apply {
-            startActivity(this)
-        }
+//        val webIntent: Intent = Intent(/* action = */
+//            ACTION_VIEW, /* uri = */
+//            Uri.parse("https://www.yahoo.com")
+//        ).apply {
+//            startActivity(this)
+//        }
+        var hIntent:Intent = Intent(this,HomeActivity::class.java)
+        startActivity(hIntent)
     }
+
+//    //chick came out of the egg --activity is visible to the user to interact - click
+//    override fun onStart() {
+//        super.onStart()
+//        Log.e(TAG, "activity is started")
+//    }
+//
+//    //sleep night -- incoming call ur activity goes into the background for sometime
+//    override fun onPause() {
+//        super.onPause()
+//        Log.w(TAG, "activity has paused")
+//
+//    }
+//
+//    //death of the chick -- activity no longer exists in the ram -- pushed to hdd[hibernate]
+//    override fun onStop() {
+//        super.onStop()
+//        Log.d(TAG, "activity is stopped")
+//
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.v(TAG, "activity is destroyed")
+//    }
 }
